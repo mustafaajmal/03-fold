3ff38b5831815732c7c9fb78eceafdd45d116483
Mustafa Ajmal <116605729+mustafaajmal@users.noreply.github.com>
2023-11-06 07:47:19 +0000

    finished assignment

--- src/Hw3.hs
+++ src/Hw3.hs
@@ -42,9 +42,9 @@ foldLeft = foldl'
 sqSum :: [Int] -> Int
 sqSum xs = foldLeft f base xs
   where
-   f a x = error "TBD: sqSum f"
-   base  = error "TBD: sqSum base"
-
+   f a x = a + x * x
+   base = 0
+   
 --------------------------------------------------------------------------------
 -- | `pipe [f1,...,fn] x` should return `f1(f2(...(fn x)))`
 --
@@ -58,10 +58,10 @@ sqSum xs = foldLeft f base xs
 -- 24
 
 pipe :: [(a -> a)] -> (a -> a)
-pipe fs   = foldLeft f base fs
+pipe fs = foldLeft f base fs
   where
-    f a x = error "TBD: pipe: f"
-    base  = error "TBD: pipe: base"
+    f acc g = acc . g
+    base = id
 
 --------------------------------------------------------------------------------
 -- | `sepConcat sep [s1,...,sn]` returns `s1 ++ sep ++ s2 ++ ... ++ sep ++ sn`
@@ -76,12 +76,12 @@ pipe fs   = foldLeft f base fs
 -- "a#b#c#d#e"
 
 sepConcat :: String -> [String] -> String
-sepConcat sep []     = ""
-sepConcat sep (x:xs) = foldLeft f base l
+sepConcat sep xs = foldLeft f base xs
   where
-    f a x            = error "TBD:sepConcat:f"
-    base             = error "TBD:sepConcat:base"
-    l                = error "TBD:l"
+    f a "" = a
+    f "" b = b
+    f a b = a ++ sep ++ b
+    base = ""
 
 intString :: Int -> String
 intString = show
@@ -100,7 +100,7 @@ intString = show
 -- "[[1, 2, 3], [4, 5], [6], []]"
 
 stringOfList :: (a -> String) -> [a] -> String
-stringOfList f xs = error "TBD:stringOfList"
+stringOfList f xs = "[" ++ sepConcat ", " (map f xs) ++ "]"
 
 --------------------------------------------------------------------------------
 -- | `clone x n` returns a `[x,x,...,x]` containing `n` copies of `x`
@@ -112,7 +112,7 @@ stringOfList f xs = error "TBD:stringOfList"
 -- ["foo", "foo"]
 
 clone :: a -> Int -> [a]
-clone x n = error "TBD:clone"
+clone x n = foldl' (\acc _ -> acc ++ [x]) [] [1..n]
 
 type BigInt = [Int]
 
@@ -128,7 +128,12 @@ type BigInt = [Int]
 -- ([1,0,0,2], [0,0,9,9])
 
 padZero :: BigInt -> BigInt -> (BigInt, BigInt)
-padZero l1 l2 = error "TBD:padZero"
+padZero l1 l2
+  | len1 > len2 = (l1, clone 0 (len1 - len2) ++ l2)
+  | otherwise = (clone 0 (len2 - len1) ++ l1, l2)
+  where
+    len1 = length l1
+    len2 = length l2
 
 --------------------------------------------------------------------------------
 -- | `removeZero ds` strips out all leading `0` from the left-side of `ds`.
@@ -143,8 +148,7 @@ padZero l1 l2 = error "TBD:padZero"
 -- []
 
 removeZero :: BigInt -> BigInt
-removeZero ds = error "TBD:removeZero"
-
+removeZero = dropWhile (== 0)
 
 --------------------------------------------------------------------------------
 -- | `bigAdd n1 n2` returns the `BigInt` representing the sum of `n1` and `n2`.
@@ -156,14 +160,14 @@ removeZero ds = error "TBD:removeZero"
 -- [1, 0, 9, 9, 8]
 
 bigAdd :: BigInt -> BigInt -> BigInt
-bigAdd l1 l2     = removeZero res
+bigAdd l1 l2 = removeLeadingZeroes $ reverse (addLists (reverse l1) (reverse l2) 0)
   where
-    (l1', l2')   = padZero l1 l2
-    res          = foldLeft f base args
-    f a x        = error "TBD:bigAdd:f"
-    base         = error "TBD:bigAdd:base"
-    args         = error "TBD:bigAdd:args"
+    addLists [] [] carry = if carry > 0 then [carry] else []
+    addLists (x:xs) [] carry = let sum = x + carry in (sum `mod` 10) : addLists xs [] (sum `div` 10)
+    addLists [] (y:ys) carry = let sum = y + carry in (sum `mod` 10) : addLists [] ys (sum `div` 10)
+    addLists (x:xs) (y:ys) carry = let sum = x + y + carry in (sum `mod` 10) : addLists xs ys (sum `div` 10)
 
+    removeLeadingZeroes = dropWhile (== 0)
 
 --------------------------------------------------------------------------------
 -- | `mulByInt i n` returns the result of multiplying
@@ -173,7 +177,16 @@ bigAdd l1 l2     = removeZero res
 -- [8,9,9,9,1]
 
 mulByInt :: Int -> BigInt -> BigInt
-mulByInt i n = error "TBD:mulByInt"
+mulByInt _ [] = []
+mulByInt 0 _ = [0]
+mulByInt i n = removeZero (reverse (mulByInt' (reverse n) i 0))
+  where
+    mulByInt' [] _ c = if c == 0 then [] else [c]
+    mulByInt' (x:xs) i c = let product = x * i + c
+                               newDigit = product `mod` 10
+                               newCarry = product `div` 10
+                           in newDigit : mulByInt' xs i newCarry
+    removeZero = dropWhile (== 0)
 
 --------------------------------------------------------------------------------
 -- | `bigMul n1 n2` returns the `BigInt` representing the product of `n1` and `n2`.
@@ -185,9 +198,10 @@ mulByInt i n = error "TBD:mulByInt"
 -- [9,9,9,9,8,0,0,0,0,1]
 
 bigMul :: BigInt -> BigInt -> BigInt
-bigMul l1 l2 = res
+bigMul [] _ = [0]
+bigMul _ [] = [0]
+bigMul l1 l2 = foldl' (\acc (product, zeros) -> bigAdd acc (product ++ zeros)) [] productsWithZeros
   where
-    (_, res) = foldLeft f base args
-    f a x    = error "TBD:bigMul:f"
-    base     = error "TBD:bigMul:base"
-    args     = error "TBD:bigMul:args"
+    productsWithZeros = zipWith (\d idx -> (mulByInt d l2, replicate idx 0)) (reverse l1) [0..]
+    replicate n x = foldl' (\acc _ -> acc ++ [x]) [] [1..n]
+
